2c1779f4cb29a55171938756edcf33c874997c59
mshahbazq <shahbiqureshi33@gmail.com>
2022-11-01 00:41:45 +0500

    Simple React-Laravel App

---
+++ backend/database/factories/AuthorFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Author>
+ */
+class AuthorFactory extends Factory
+{
+    /**
+     *The name of the factory's corresponding model. 
+     * 
+     * @var string
+     */
+    // protected $model = Author::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name
+        ];
+    }
+}
